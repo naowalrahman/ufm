024d94344e7a59a917695c40dbc52cd684464db8
Naowal Rahman <naowal.ar@gmail.com>
2024-01-04 22:34:44 -0500

    finished get_file_listing and some tweaks

--- data.c
+++ data.c
@@ -1,10 +1,32 @@
 #include "data.h"
 
 bool create_file(char* name) {
-    // int fd = open(name, O_CREAT | O_WRONLY, 0644);
-    // if (fd == -1) {
-    //     return false;
-    // }
-    // close(fd);
-    // return true;
+    int fd = open(name, O_CREAT | O_WRONLY, 0644);
+    if (fd == -1) return false;
+    close(fd);
+    return true;
+}
+
+struct stat **get_file_listing(char *dir) {
+    DIR *d = opendir(dir);
+    struct dirent *entry;
+
+    int size = 0;
+    while((entry = readdir(d))) {
+        if(entry->d_type != DT_DIR) ++size;
+    }
+    
+    closedir(d);
+    d = opendir(dir);
+    struct stat **list = malloc(size * sizeof(struct stat));
+    struct stat **itr = list;
+    
+    while((entry = readdir(d))) {
+        if(entry->d_type != DT_DIR) {
+            stat(entry->d_name, *itr);
+            ++itr;
+        }
+    }
+
+    return list;
 }
--- data.h
+++ data.h
@@ -4,14 +4,14 @@
 
 #include <dirent.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdbool.h>
 
-struct stat **get_dir_listing(char *dir);
-bool create_file(char* name);
+struct stat **get_file_listing(char *dir);
+bool create_file(char *name);
 
 #endif // DATA
