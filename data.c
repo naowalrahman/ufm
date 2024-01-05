d4059371f35aeefa737319679b3699c2a4a96b13
RahelArka <rarka40@stuy.edu>
2024-01-04 20:32:37 -0500

    created and imported data.c/h

---
+++ data.c
@@ -0,0 +1,10 @@
+#include "data.h"
+
+bool create_file(char* name) {
+    // int fd = open(name, O_CREAT | O_WRONLY, 0644);
+    // if (fd == -1) {
+    //     return false;
+    // }
+    // close(fd);
+    // return true;
+}
